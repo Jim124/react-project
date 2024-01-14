e2f55817b388ad5a0af5b17152179d1b1dbd5510
Jim124 <fengpingan065@gmail.com>
2024-01-14 14:18:07 +0800

    implement CityList and CountryList

--- worldwild/src/app.jsx
+++ worldwild/src/app.jsx
@@ -1,11 +1,35 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
+import {useState,useEffect} from "react"
+
 import Product from "./pages/Product"
 import Homepage from "./pages/Homepage"
 import Pricing from "./pages/Pricing"
 import PageNotFound from "./pages/PageNotFound"
 import AppLayout from "./pages/AppLayout"
 import Login from "./pages/Login"
+import CityList from "./components/CityList"
+import CountryList from "./components/CountryList"
+
+const BASE_URL = "http://localhost:9000"
 function App() {
+  const [cities,setCities] = useState([]);
+  const [isLoading,setIsLoading] = useState(false);
+
+  useEffect(function(){
+    async function fetchCities(){
+      try {
+        setIsLoading(true)
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch (error) {
+        alert("there is an error");
+      }finally{
+        setIsLoading(false)
+      }
+    }
+    fetchCities()
+  },[]);
   return (
     <BrowserRouter>
       <Routes>
@@ -14,9 +38,9 @@ function App() {
         <Route path="pricing" element={<Pricing />}></Route>
         <Route path="pageNotFound" element={<PageNotFound />}></Route>
         <Route path="app" element={<AppLayout />}>
-          <Route index  element={<p>List of cities</p>} />
-          <Route path="cities" element={<p>List of cities</p>} />
-          <Route path="countries" element={<p>List of countries</p>} />
+          <Route index  element={<CityList cities={cities} isLoading={isLoading} />} />
+          <Route path="cities" element={<CityList cities={cities} isLoading={isLoading}/>} />
+          <Route path="countries" element={<CountryList cities={cities} isLoading={isLoading}/>} />
           <Route path="form" element={<p>Form</p>} />
         </Route>
         <Route path="login" element={<Login />}></Route>
---
+++ worldwild/src/components/CountryList.jsx
@@ -0,0 +1,22 @@
+import Message from "./Message"
+import Spinner from "./Spinner"
+import styles from './CountryList.module.css'
+import CountryItem from "./CountryItem"
+function CountryList({cities,isLoading}) {
+    if(isLoading) return <Spinner/>
+    if (!cities.length) return <Message  message="Add your first city by clicking on a city on the map "/>
+    const countries = cities.reduce((arr,city) => {
+        if(!arr.map(el => el.country).includes(city.country))
+        return [...arr,{country:city.country,emoji:city.emoji}]; 
+        else return arr;
+    }   ,[]);
+    cities.map(city => city.country).includes();
+    
+    return (
+        <ul className={styles.countryList}>
+            {countries.map(country => <CountryItem country={country} key={country.name} />)}
+        </ul>
+    )
+}
+
+export default CountryList
