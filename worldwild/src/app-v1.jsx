77a5f6a67cdc4db695b94edab58d083eb48d50a5
Jim124 <fengpingan065@gmail.com>
2024-01-23 22:55:15 +0800

    use Context.api refactor project

--- worldwild/src/app-v1.jsx
+++ worldwild/src/app-v1.jsx
@@ -11,13 +11,28 @@ import CityList from "./components/CityList"
 import CountryList from "./components/CountryList"
 import City from "./components/City"
 import Form from "./components/Form"
-import { CitiesProvider } from "./context/CitiesContext"
 
-
+const BASE_URL = "http://localhost:9000"
 function App() {
-  
+  const [cities,setCities] = useState([]);
+  const [isLoading,setIsLoading] = useState(false);
+
+  useEffect(function(){
+    async function fetchCities(){
+      try {
+        setIsLoading(true)
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch (error) {
+        alert("there is an error");
+      }finally{
+        setIsLoading(false)
+      }
+    }
+    fetchCities()
+  },[]);
   return (
-    <CitiesProvider>
     <BrowserRouter>
       <Routes>
         <Route index element={<Homepage />}></Route>
@@ -26,16 +41,15 @@ function App() {
         <Route path="pageNotFound" element={<PageNotFound />}></Route>
         <Route path="app" element={<AppLayout />}>
           <Route index  element={<Navigate replace to="cities"/>} />
-          <Route path="cities" element={<CityList/>} />
+          <Route path="cities" element={<CityList cities={cities} isLoading={isLoading}/>} />
           <Route path="cities/:id" element={<City />}/>
-          <Route path="countries" element={<CountryList/>} />
+          <Route path="countries" element={<CountryList cities={cities} isLoading={isLoading}/>} />
           <Route path="form" element={<Form />} />
         </Route>
         <Route path="login" element={<Login />}></Route>
         <Route path="*" element={<PageNotFound/> }></Route>
       </Routes>
     </BrowserRouter>
-    </CitiesProvider>
   )
 }
 
--- worldwild/src/components/CountryList.jsx
+++ worldwild/src/components/CountryList.jsx
@@ -2,7 +2,9 @@ import Message from "./Message"
 import Spinner from "./Spinner"
 import styles from './CountryList.module.css'
 import CountryItem from "./CountryItem"
-function CountryList({cities,isLoading}) {
+import { useCities } from "../context/CitiesContext"
+function CountryList() {
+    const {isLoading,cities} = useCities();
     if(isLoading) return <Spinner/>
     if (!cities.length) return <Message  message="Add your first city by clicking on a city on the map "/>
     const countries = cities.reduce((arr,city) => {
