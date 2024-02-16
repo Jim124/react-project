1cd7afbe269efea8e197bebe14727aee0e9b054e
Jim124 <fengpingan065@gmail.com>
2024-02-16 13:57:43 +0700

    use redus-thunk

--- redux-intro/src/features/accounts/AccountOperations.js
+++ redux-intro/src/features/accounts/AccountOperations.js
@@ -1,19 +1,38 @@
 import { useState } from "react";
-
+import { useDispatch, useSelector } from "react-redux";
+import {deposit, payLoan, requestLoan, withdraw} from "./accountSlice"
 function AccountOperations() {
   const [depositAmount, setDepositAmount] = useState("");
   const [withdrawalAmount, setWithdrawalAmount] = useState("");
   const [loanAmount, setLoanAmount] = useState("");
   const [loanPurpose, setLoanPurpose] = useState("");
   const [currency, setCurrency] = useState("USD");
+  const dispatch = useDispatch();
+  const account = useSelector(store => store.account);
+  const {loan:currentLoan,loanPurpose:currentPurpose,balance,isLoading} = account;
+  console.log(balance)
+  function handleDeposit() {
+    if(!depositAmount) return;
+    dispatch(deposit(depositAmount,currency));
+    setDepositAmount("");
+  }
 
-  function handleDeposit() {}
+  function handleWithdrawal() {
+    if(!withdrawalAmount) return;
+    dispatch(withdraw(withdrawalAmount));
+    setWithdrawalAmount("");
+  }
 
-  function handleWithdrawal() {}
+  function handleRequestLoan() {
+    if(!loanAmount || !loanPurpose)return;
+    dispatch(requestLoan(loanAmount,loanPurpose));
+    setLoanAmount("");
+    setLoanPurpose("");
+  }
 
-  function handleRequestLoan() {}
-
-  function handlePayLoan() {}
+  function handlePayLoan() {
+    dispatch(payLoan());
+  }
 
   return (
     <div>
@@ -35,7 +54,7 @@ function AccountOperations() {
             <option value="GBP">British Pound</option>
           </select>
 
-          <button onClick={handleDeposit}>Deposit {depositAmount}</button>
+          <button onClick={handleDeposit} disabled={isLoading}>{isLoading ? `Converting`:`Deposit ${depositAmount}`}</button>
         </div>
 
         <div>
@@ -65,11 +84,11 @@ function AccountOperations() {
           />
           <button onClick={handleRequestLoan}>Request loan</button>
         </div>
-
-        <div>
-          <span>Pay back $X</span>
+      {currentLoan > 0 && <div>
+          <span>Pay back ${currentLoan} {currentPurpose}</span>
           <button onClick={handlePayLoan}>Pay loan</button>
-        </div>
+        </div>}
+        
       </div>
     </div>
   );
--- redux-intro/src/store.js
+++ redux-intro/src/store.js
@@ -1,27 +1,11 @@
-import { createStore,combineReducers } from "redux";
-
+import { createStore,combineReducers, applyMiddleware } from "redux";
+import {thunk}  from "redux-thunk";
 import accountReducer from "./features/accounts/accountSlice";
 import customerReducer from "./features/customers/customerSlice";
 const rootReducer = combineReducers({
     account:accountReducer,
     customer:customerReducer
 });
-const store = createStore(rootReducer);
-// store.dispatch({type:"account/deposit",payload:500});
-// store.dispatch({type:"account/withdraw",payload:200});
-// store.dispatch({type:"account/requestLoan",payload:{amount:1000,purpose:"buy a car"}});
-// store.dispatch({type:"account/payLoan"});
-// console.log(store.getState());
+const store = createStore(rootReducer,applyMiddleware(thunk));
 
-
-// store.dispatch(deposit(500));
-// store.dispatch(withdraw(200));
-// store.dispatch(requestLoan(1000,"Buy a cheap car"));
-// store.dispatch(payLoan());
-// console.log(store.getState());
-
-
-
-// store.dispatch(createCustomer("Jim",23242452));
-// console.log(store.getState())
 export default store;
