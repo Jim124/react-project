024fafd15e1e29a3ee416533d9b43e70094926fd
Jim124 <fengpingan065@gmail.com>
2024-07-13 12:06:12 +0800

    use Modal component

--- WorldwideBack/src/features/cabins/CreateCabinForm.jsx
+++ WorldwideBack/src/features/cabins/CreateCabinForm.jsx
@@ -10,36 +10,51 @@ import FormRow from '../../ui/FormRow';
 import { useCreateCabin } from './useCreateCabin';
 import { useUpdateCabin } from './useUpdateCabin';
 
-function CreateCabinForm({ cabinToEdit = {} }) {
+function CreateCabinForm({ cabinToEdit = {}, onCloseModal }) {
   const { id: editId, ...editValues } = cabinToEdit;
+
   const isEditSession = Boolean(editId);
+
   const { register, handleSubmit, reset, formState, getValues } = useForm({
     defaultValues: isEditSession ? editValues : {},
   });
+
   const { errors } = formState;
 
   const { isCreating, createCabin } = useCreateCabin();
   const { isEditing, editCabin } = useUpdateCabin();
 
   const isWorking = isCreating || isEditing;
+
   function onSubmit(data) {
     const image = typeof data.image === 'string' ? data.image : data.image[0];
     const newCabin = { ...data, image: image };
     if (isEditSession)
       editCabin(
         { newCabin: newCabin, id: editId },
-        { onSuccess: () => reset() }
+        {
+          onSuccess: (data) => {
+            reset();
+            onCloseModal?.();
+          },
+        }
       );
     else
       createCabin(newCabin, {
-        onSuccess: (data) => reset(),
+        onSuccess: (data) => {
+          reset();
+          onCloseModal?.();
+        },
       });
   }
   function onError(errors) {
     // console.log(errors);
   }
   return (
-    <Form onSubmit={handleSubmit(onSubmit, onError)}>
+    <Form
+      onSubmit={handleSubmit(onSubmit, onError)}
+      type={onCloseModal ? 'modal' : 'regular'}
+    >
       <FormRow label={'Cabin name'} error={errors?.name?.message}>
         <Input
           type='text'
@@ -108,7 +123,11 @@ function CreateCabinForm({ cabinToEdit = {} }) {
       </FormRow>
       <FormRow>
         {/* type is an HTML attribute! */}
-        <Button variation='secondary' type='reset'>
+        <Button
+          variation='secondary'
+          type='reset'
+          onClick={() => onCloseModal?.()}
+        >
           Cancel
         </Button>
         <Button disabled={isWorking}>
