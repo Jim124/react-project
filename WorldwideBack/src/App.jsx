eeae047427c1f8d6a09013e441a3c787cada058c
Hongfei01 <hongfeidu3@gmail.com>
2024-07-21 18:21:26 +0800

    modify bookingDetail component

--- WorldwideBack/src/App.jsx
+++ WorldwideBack/src/App.jsx
@@ -13,6 +13,7 @@ import Login from './pages/Login';
 import PageNotFound from './pages/PageNotFound';
 import AppLayout from './ui/AppLayout';
 import { Toaster } from 'react-hot-toast';
+import Booking from './pages/Booking';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -31,6 +32,8 @@ function App() {
             <Route index element={<Navigate replace to='dashboard' />} />
             <Route path='dashboard' element={<Dashboard />} />
             <Route path='bookings' element={<Bookings />} />
+            <Route path='bookings/:bookingId' element={<Booking />} />
+
             <Route path='cabins' element={<Cabins />} />
             <Route path='users' element={<Users />} />
             <Route path='settings' element={<Settings />} />
@@ -50,7 +53,7 @@ function App() {
             duration: 3000,
           },
           error: {
-            duration: 5000,
+            duration: 3000,
           },
           style: {
             fontSize: '16px',
